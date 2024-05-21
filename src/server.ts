8609a25ed9f7f121540fbd391e21e60af3e432f2
Brian Joseph <brianjosephstudio@gmail.com>
2024-05-21 17:47:11 +0200

    work on server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,23 @@ import express from 'express'
 import { router } from './router'
 import cors from 'cors'
 import path from 'path'
+import bodyParser from 'body-parser'
 
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.json())
+app.use("/",(
+    request,
+    _response,
+    next
+) => {
+    console.log("New Request:")
+    console.log(request.url)
+    next()
+})
 app.use("/api", router)
+app.get("/health",(req,res)=> res.send("ok"))
 app.use(express.static(path.resolve(__dirname, "..", "public")))
 
 const port = process.env.PORT
