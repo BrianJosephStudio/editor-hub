b9b3800f72cc3e66a8d8b030cda6f2bdc72d010c
Brian Joseph <brianjosephstudio@gmail.com>
2024-05-22 11:48:11 +0200

    created dropboxProxy

---
+++ src/util/middleware/dropboxProxy.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { createProxyMiddleware, responseInterceptor } from 'http-proxy-middleware';
+
+const handleProxyErrors = (err: any, req: Request, res: Response | any) => {
+  console.error('Proxy error:', err);
+  if (res.status !== null) {
+    res.status(500).json({ error: 'Proxy error', details: err.message });
+  } else {
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Proxy error', details: err.message }));
+  }
+};
+
+export const createDropboxProxy = (target: string,) => createProxyMiddleware({
+  target: target,
+  changeOrigin: true,
+  selfHandleResponse: true,
+  on: {
+    error: handleProxyErrors
+  },
+});
