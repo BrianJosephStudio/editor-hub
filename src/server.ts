3ddb59b9f2edbb13c7afcf7059735ef997aa5335
Brian Joseph <brianjosephstudio@gmail.com>
2024-05-23 16:38:22 +0200

    Serving new readme file on root endpoint

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,10 @@ app.use("/api", router)
 app.get("/health",(req,res)=> res.send("ok"))
 app.use(express.static(path.resolve(__dirname, "..", "public")))
 
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "public", "README.md"))
+})
+
 const port = process.env.PORT
 
 app.listen(port, () => {
