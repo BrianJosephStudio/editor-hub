4582a6cbc1605781513547157a69947947d00502
Brian Joseph Studio <brianjosephstudio@gmail.com>
2024-05-31 16:47:44 +0200

    create-project-folder endpoint was created

--- src/api/dropbox.ts
+++ src/api/dropbox.ts
@@ -1,6 +1,8 @@
 import { getAccessToken } from "../util/dropbox/accessToken"
 import { createDropboxProxy } from '../util/middleware/dropboxProxy'
 import { Request, Response, NextFunction } from "express";
+import { ProjectData, ProjectType } from "../types/ProjectManager.d";
+import axios from "axios";
 
 export const addAuthorizationHeader = async (req: Request, _res: Response, next: NextFunction) => {
     const { access_token } = await getAccessToken();
@@ -31,5 +33,66 @@ export const dropboxApiProxy = async () => {
         return proxyMiddleware
     } catch (e: any) {
         throw e
+    }
+}
+
+export const getProjectPathByDate = () => {}
+export const doesProjectPathByDateExistp = () => {}
+export const isProjectPathByMonthClosed  = () => {} //returns whether the folder is closed and also whether it needs to be closed
+export const closeProjectFolderByDate = () => {}
+export const createProjectFolderByDate = () => {}
+
+export const createProjectFolder = async (req: Request<{}, {}, ProjectData>, res: Response) => {
+    try {
+        const { projectName, projectNumber, projectType } = req.body
+
+        const createFoldersurl = 'https://api.dropboxapi.com/2/files/create_folder_batch'
+        const getFolderLinkUrl = 'https://api.dropboxapi.com/2/sharing/create_shared_link_with_settings'
+        const createFileRequestUrl = 'https://api.dropboxapi.com/2/file_requests/create'
+
+        const rootFolder = "Guides"
+
+        const projectId = `${projectType === ProjectType.SHORT ? "S" : ""}${projectNumber}`
+        const rootProjectFolderName = `${projectId} - ${projectName}`
+        const uploadsFolderName = `File Requests`
+        const rootProjectFolderPath = `/${rootFolder}/${rootProjectFolderName}`
+        const uploadsFolderPath = `${rootProjectFolderPath}/${uploadsFolderName}`
+
+        const headers = {
+            Authorization: req.headers.Authorization
+        }
+
+        const createRequestBody = {
+            paths: [
+                rootProjectFolderPath,
+                uploadsFolderPath,
+            ]
+        }
+
+        await axios.post(createFoldersurl, createRequestBody, { headers })
+        
+        const getFolderLInkBody = {
+            path: rootProjectFolderPath
+        }
+        const createFileRequestBody = {
+            title: rootProjectFolderName,
+            destination: uploadsFolderPath
+        }
+        
+        const { data: getFolderLinkData } = await axios.post(getFolderLinkUrl, getFolderLInkBody, { headers })
+        const { data: createRequestFileData } = await axios.post(createFileRequestUrl, createFileRequestBody, { headers })
+
+        
+        
+        const uploadLink = createRequestFileData.url
+        const projectLink = getFolderLinkData.url
+
+        const response = {
+            projectLink,
+            uploadLink
+        }
+        res.json(response)
+    } catch (e) {
+        console.error(e)
     }
 }
