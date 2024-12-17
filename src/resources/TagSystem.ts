99338ea46670dcf946ffbdc529886724e9bc4d53
Brian Ure <brianjosephstudio@gmail.com>
2024-12-17 18:06:22 +0100

    fixed Vyse's keybind

--- src/resources/TagSystem.ts
+++ src/resources/TagSystem.ts
@@ -24,7 +24,7 @@ export const AgentTags: TagObject[] = [
     {tag:"skye",displayName:"Skye",description:"Player is playing Skye", unique: true, timeless: true, protected: true, keybind: "s", id: "ag020"},
     {tag:"sova",displayName:"Sova",description:"Player is playing Sova", unique: true, timeless: true, protected: true, keybind: "z", id: "ag021"},
     {tag:"viper",displayName:"Viper",description:"Player is playing Viper", unique: true, timeless: true, protected: true, keybind: "v", id: "ag022"},
-    {tag:"vyse",displayName:"Vyse",description:"Player is playing Vyse", unique: true, timeless: true, protected: true, keybind: "v", id: "ag025"},
+    {tag:"vyse",displayName:"Vyse",description:"Player is playing Vyse", unique: true, timeless: true, protected: true, keybind: "f", id: "ag025"},
     {tag:"yoru",displayName:"Yoru",description:"Player is playing Yoru", unique: true, timeless: true, protected: true, keybind: "y", id: "ag023"}
 ]
 export const MapTags: TagObject[] = [
