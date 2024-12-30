7c58e637616dc8b4062473560e3188b0402c6d93
Brian Ure <brianjosephstudio@gmail.com>
2024-12-30 18:42:05 +0100

    formatting

--- apps/clip-tagger/src/pages/clip-tagger/ClipTagger.tsx
+++ apps/clip-tagger/src/pages/clip-tagger/ClipTagger.tsx
@@ -6,93 +6,96 @@ import { TagsDisplay } from "../../pages/clip-tagger/components/tags-display/Tag
 import { useAppContext } from "../../context/AppContext"
 import { useKeybind } from "../../context/KeyBindContext"
 import { useClipViewer } from "../../context/ClipViewerContext"
-import { TagsProvider } from "../../context/TagsContext"
+import { useTags } from "../../context/TagsContext"
 import { useFolderNavigation } from "../../context/FolderNavigationContext"
 import { Settings } from "./components/settings/Settings"
 
 export const ClipTagger = () => {
-	const { AppRoot } = useAppContext()
+  const { AppRoot } = useAppContext()
 
-	const { getActiveItem, focusNextItem, focusPreviousItem, handleBackNavigation } = useFolderNavigation()
-	const { videoPlayer, skipTime } = useClipViewer()
-	const { setBlockGroupLevelListeners } = useKeybind();
+  const { getActiveItem, focusNextItem, focusPreviousItem, handleBackNavigation } = useFolderNavigation()
+  const { videoPlayer, skipTime } = useClipViewer()
+  const { setBlockGroupLevelListeners } = useKeybind();
+  const { removeLastAddedTag } = useTags()
 
-	return (
-		<TagsProvider>
-			<Box
-				component={"div"}
-				data-testid={"page:clip-tagger:root"}
-				ref={AppRoot}
-				tabIndex={0}
-				sx={{
-					display: "flex",
-					width: "100vw",
-					minHeight: 0,
-					flexGrow: 1,
-					outline: "none",
-				}}
+  return (
+    <Box
+      component={"div"}
+      data-testid={"page:clip-tagger:root"}
+      ref={AppRoot}
+      tabIndex={0}
+      sx={{
+        display: "flex",
+        width: "100vw",
+        minHeight: 0,
+        flexGrow: 1,
+        outline: "none",
+      }}
 
-				onKeyDown={(event) => {
-					const { key, altKey } = event
-					if ((altKey && (key === "j" || event.key === '∆')) || key === 'ArrowDown') {
-						event.stopPropagation()
-						focusNextItem();
-					}
-					if ((altKey && (key === "k" || event.key === '˚')) || key === 'ArrouwUp') {
-						event.stopPropagation()
-						focusPreviousItem();
-					}
-					if (altKey && (key === "h" || event.key === '∫')) {
-						if (!videoPlayer.current) return;
-						event.stopPropagation()
-						const newTime = videoPlayer.current.currentTime - (skipTime / 1000);
-						videoPlayer.current.currentTime = newTime
-					}
-					if (altKey && (key === "l" || event.key === 'ç')) {
-						if (!videoPlayer.current) return;
-						event.stopPropagation()
-						const newTime = videoPlayer.current.currentTime + (skipTime / 1000);
-						videoPlayer.current.currentTime = newTime
-					}
-					if (key === "Backspace") {
-						event.stopPropagation()
-						handleBackNavigation(1);
-					}
-					if (key === " ") {
-						videoPlayer.current?.paused
-							? videoPlayer.current?.play()
-							: videoPlayer.current?.pause();
-					}
-					if (key === "Escape") setBlockGroupLevelListeners(false);
-					if (key === "Enter") {
-						const activeItem = getActiveItem()
-						if (!activeItem) return;
-						activeItem.dispatchEvent(new KeyboardEvent("keydown", {
-							key: "Enter",
-							code: "Enter",
-							keyCode: 13,
-							charCode: 13,
-							bubbles: true,
-							cancelable: true,
-						}))
-					}
-				}}
-			>
-				<ClipBrowser></ClipBrowser>
-				<Box
-					sx={{
-						display: "flex",
-						flexDirection: 'column',
-						minWidth: "0",
-						flexGrow: 1
-					}}
-				>
-					<Settings></Settings>
-					<ClipViewer></ClipViewer>
-					<TagsManager></TagsManager>
-				</Box>
-				<TagsDisplay></TagsDisplay>
-			</Box>
-		</TagsProvider>
-	)
+      onKeyDown={(event) => {
+        const { key, altKey } = event
+        if ((altKey && (key === "j" || event.key === '∆')) || key === 'ArrowDown') {
+          event.stopPropagation()
+          focusNextItem();
+        }
+        if ((altKey && (key === "k" || event.key === '˚')) || key === 'ArrouwUp') {
+          event.stopPropagation()
+          focusPreviousItem();
+        }
+        if (altKey && (key === "h" || event.key === '∫')) {
+          if (!videoPlayer.current) return;
+          event.stopPropagation()
+          const newTime = videoPlayer.current.currentTime - (skipTime / 1000);
+          videoPlayer.current.currentTime = newTime
+        }
+        if (altKey && (key === "l" || event.key === 'ç')) {
+          if (!videoPlayer.current) return;
+          event.stopPropagation()
+          const newTime = videoPlayer.current.currentTime + (skipTime / 1000);
+          videoPlayer.current.currentTime = newTime
+        }
+        if (altKey && (key === "u" || event.key === '¨')) {
+          event.stopPropagation()
+          removeLastAddedTag()
+        }
+        if (key === "Backspace") {
+          event.stopPropagation()
+          handleBackNavigation(1);
+        }
+        if (key === " ") {
+          videoPlayer.current?.paused
+            ? videoPlayer.current?.play()
+            : videoPlayer.current?.pause();
+        }
+        if (key === "Escape") setBlockGroupLevelListeners(false);
+        if (key === "Enter") {
+          const activeItem = getActiveItem()
+          if (!activeItem) return;
+          activeItem.dispatchEvent(new KeyboardEvent("keydown", {
+            key: "Enter",
+            code: "Enter",
+            keyCode: 13,
+            charCode: 13,
+            bubbles: true,
+            cancelable: true,
+          }))
+        }
+      }}
+    >
+      <ClipBrowser></ClipBrowser>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: 'column',
+          minWidth: "0",
+          flexGrow: 1
+        }}
+      >
+        <Settings></Settings>
+        <ClipViewer></ClipViewer>
+        <TagsManager></TagsManager>
+      </Box>
+      <TagsDisplay></TagsDisplay>
+    </Box>
+  )
 }
