eb1412198ad27e7d932a7234089df4d755c7b5ca
Brian Ure <brianjosephstudio@gmail.com>
2025-01-06 11:35:26 +0100

    fixed server logging when checking for roles

--- server/src/middleware/userAuthorization.ts
+++ server/src/middleware/userAuthorization.ts
@@ -9,11 +9,11 @@ export const clipTaggerAuthorizationFilter = (
   const auth = getAuth(req);
 
   const roles = auth.sessionClaims?.metadata?.roles ?? [];
-  const authorizedRoles = ["admin", "editor", "animator"]
+  const authorizedRoles = ["admin", "editor", "animator", 'clip_tagger']
 
   const isAuthorized = roles.some(role => authorizedRoles.some(authorizedRole => authorizedRole === role));
 
-  console.log("API v2 request by role:", auth.sessionClaims?.metadata?.role);
+  console.log("API v2 request by user with roles:", auth.sessionClaims?.metadata?.roles);
 
   if (!isAuthorized) {
     return res.status(403).send("Unauthorized");
